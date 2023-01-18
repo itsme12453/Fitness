45dbf065fbd658be73ca26af1b7b786923fe2422
itsme12453 <heetvirola110508@gmail.com>
2023-01-18 17:21:41 +0000

    Setup

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useState, useEffect } from 'react';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'SourceSansPro-Bold': require('./assets/fonts/SourceSansPro-Bold.ttf'),
+    'SourceSansPro-SemiBold': require('./assets/fonts/SourceSansPro-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.title}>Login</Text>
     </View>
   );
 }
@@ -14,7 +34,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 25,
+    marginTop: 200,
   },
+  title: {
+    fontSize: RFPercentage(5),
+    fontFamily: "SourceSansPro-SemiBold",
+    color: "#1D304E",
+    marginBottom: 20,
+  },
+  // input: {
+  //   borderColor: "#fff",
+  //   borderBottomColor: "#e3e3e3",
+  //   backgroundColor: "none",
+  //   height: 100,
+  // }
 });
